c42b420d80d62ea91fdeedb6db98ff15fef3ecf3
elliot-winch <erw331@nyu.edu>
2019-01-27 16:50:34 -0500

    Audio Added

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Grandma;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public Damageable d;
+    public PlayRandom audio;
+
+    private void Awake()
+    {
+        d.OnDestroyed += () =>
+        {
+            audio.PlayRand();
+        };
+    }
+}
