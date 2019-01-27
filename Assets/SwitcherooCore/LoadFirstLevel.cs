4f81774728f2cf7ba866f5ceed245ec2c4feb864
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-01-27 15:01:03 -0500

    added stuff

---
+++ Assets/SwitcherooCore/LoadFirstLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstLevel : MonoBehaviour
+{
+    public void FirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
