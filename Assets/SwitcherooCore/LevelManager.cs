3ef7f627fb7bdb0b22670970d0692fa3b11d425e
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-01-27 07:52:46 -0500

    testlevel

---
+++ Assets/SwitcherooCore/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Grandma;
+
+
+public class LevelManager : GrandmaComponent
+{
+    public static LevelManager Instance { get; private set; }
+
+    public int currentLevel;
+    protected override void Start()
+    {
+        base.Start();
+        Instance = this;
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+
+}
