fe6cf8c8c0bf52394093eb31622b1325683d5631
elliot-winch <erw331@nyu.edu>
2019-01-27 14:57:26 -0500

    Switch UI Done

---
+++ Assets/SwitcherooCore/UI/SwitchCursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCursor : MonoBehaviour
+{
+    public float angularSpeed;
+
+    private void Awake()
+    {
+        StartCoroutine(Rotate());
+    }
+
+    void Update()
+    {
+        var screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(screenPos.x, screenPos.y, 0f);
+    }
+
+    IEnumerator Rotate()
+    {
+        float angle = 0;
+
+        while (true)
+        {
+            angle = (angle + (angularSpeed * Time.deltaTime)) % 360f;
+
+            transform.Rotate(new Vector3(0f, 0f, angle));
+
+            yield return null;
+        }
+    }
+}
