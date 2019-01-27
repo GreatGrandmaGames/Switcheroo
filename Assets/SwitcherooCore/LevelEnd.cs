600e6c4fea7f56ac874454832c019af7ede5a567
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-01-27 13:32:54 -0500

    workin on stuff

---
+++ Assets/SwitcherooCore/LevelEnd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Grandma;
+
+public class LevelEnd : Interactable
+{
+    protected override void OnTriggered(string triggeringID)
+    {
+        LevelManager.Instance.NextLevel();
+    }
+}
--- Assets/SwitcherooCore/LevelManager.cs
+++ Assets/SwitcherooCore/LevelManager.cs
@@ -10,14 +10,25 @@ public class LevelManager : GrandmaComponent
     public static LevelManager Instance { get; private set; }
 
     public int currentLevel;
+
+    public Damageable player;
+
+
     protected override void Start()
     {
         base.Start();
         Instance = this;
+        player.OnDestroyed += Reload;
     }
 
     public void Reload()
     {
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    public void NextLevel()
+    {
+        currentLevel++;
         SceneManager.LoadScene(currentLevel);
     }
 
